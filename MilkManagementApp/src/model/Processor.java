4fd0c51eb3ee4c5319314777b8bb4b533d8cece5
CesarAcero97 <56569713+CesarAcero97@users.noreply.github.com>
2021-04-01 23:28:08 -0500

    Reporte 1 creado como HashMap<String, Integer>

--- MilkManagementApp/src/model/Processor.java
+++ MilkManagementApp/src/model/Processor.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Processor {
 	private ArrayList<ProductionEntry> entryList;
@@ -27,6 +28,31 @@ public class Processor {
 			fullList.add(entry.toObjectVector());
 		}
 		return fullList;
+	}
+	
+	public HashMap<String, Integer> generateReport1(int year) {
+		//Grado de producción en relación al tipo de explotación(Lechería tradicional y Lechería especializada). En general para todos los municipios
+		HashMap<String, Integer> productionPerTown = new HashMap<String, Integer>();
+		for (ProductionEntry entry : entryList) {
+			int totalProduction1 = 0;
+			int totalProduction2 = 0;
+			int totalProduction3 = 0;
+			switch (entry.getFarmingType().getType()) {
+			case "LECHERIA TRADICIONAL":
+				totalProduction1 +=  entry.getDailyLiters();
+				productionPerTown.put(entry.getFarmingType().getType(), totalProduction1);
+				break;
+			case "LECHERIA ESPECIALIZADA":
+				totalProduction2 +=  entry.getDailyLiters();
+				productionPerTown.put(entry.getFarmingType().getType(), totalProduction2);
+				break;
+			case "DOBLE PROPOSITO":
+				totalProduction3 +=  entry.getDailyLiters();
+				productionPerTown.put(entry.getFarmingType().getType(), totalProduction3);
+				break;
+			}
+		}
+		return productionPerTown;
 	}
 
 }
--- MilkManagementApp/src/persistence/JsonHandler.java
+++ MilkManagementApp/src/persistence/JsonHandler.java
@@ -1,8 +1,14 @@
 package persistence;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 import com.github.cliftonlabs.json_simple.JsonArray;
@@ -33,6 +39,36 @@ public class JsonHandler {
 		return personList;
 	}
 	
+	public static String readFileFromUrl(String url) {
+        InputStream is = null;
+        try {
+            is = new URL(url).openStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                return readAll(rd);
+            } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "";
+    }
 	
+	private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1)
+            sb.append((char) cp);
+        String result = sb.toString();
+        return result.replaceAll("\\\\n", "");
+    }
 	
 }
