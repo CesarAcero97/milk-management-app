6aa6bc5740f8a2c22f335a35354ac756297482e6
CesarAcero97 <56569713+CesarAcero97@users.noreply.github.com>
2021-04-09 19:06:49 -0500

    Utilidades actualizadas

--- MilkManagementApp/src/utils/Utilities.java
+++ MilkManagementApp/src/utils/Utilities.java
@@ -7,9 +7,17 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import model.*;
-import persistence.LanguageHandler;
-
+import persistence.HandlerLanguage;
+	
 public class Utilities {
+	
+	private static final String TEXT_TOTAL_LITERS_PER_DAY = "Total_Liters_Per_Day";
+	private static final String TEXT_NUMBER_COWS_FOR_MILKING = "Number_Cows_For_Milking";
+	private static final String TEXT_PRODUCTION_PER_COW = "Production_Per_Cow";
+	private static final String TEXT_TYPE_OF_EXPLOITATION = "Type_Of_Exploitation";
+	private static final String TEXT_NAME_MUNICIPALITY = "Name_Municipality";
+	private static final String TEXT_YEAR_SELECTION = "Year_Selection";
+	private static final String TEXT_IDENTIFIER = "Identifier";
 
 	public static final FarmingType parseFarmingType(String farmingType) {
 		FarmingType type = null;
@@ -42,7 +50,19 @@ public class Utilities {
 	}
 	
 	public static String generateProperty(String vector) {
-		return LanguageHandler.languageProperties.getProperty(vector);
+		return HandlerLanguage.languageProperties.getProperty(vector);
+	}
+	
+	public static String[] changeLanguageHeaders() {
+		String[] headers = new String[7];
+		headers[0] = HandlerLanguage.languageProperties.getProperty(TEXT_IDENTIFIER);
+		headers[1] = HandlerLanguage.languageProperties.getProperty(TEXT_YEAR_SELECTION);
+		headers[2] = HandlerLanguage.languageProperties.getProperty(TEXT_NAME_MUNICIPALITY);
+		headers[3] = HandlerLanguage.languageProperties.getProperty(TEXT_TYPE_OF_EXPLOITATION);
+		headers[4] = HandlerLanguage.languageProperties.getProperty(TEXT_PRODUCTION_PER_COW);
+		headers[5] = HandlerLanguage.languageProperties.getProperty(TEXT_NUMBER_COWS_FOR_MILKING);
+		headers[6] = HandlerLanguage.languageProperties.getProperty(TEXT_TOTAL_LITERS_PER_DAY);
+		return headers;
 	}
 
 }
