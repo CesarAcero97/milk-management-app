db32cf60d713fa0f3d7c05d2eb5251e1c85a89c2
CesarAcero97 <56569713+CesarAcero97@users.noreply.github.com>
2021-05-04 10:23:25 -0500

    Versión inicial de manejador de archivos de texto

--- MilkManagementApp/src/persistence/TextManager.java
+++ MilkManagementApp/src/persistence/TextManager.java
@@ -1,5 +1,43 @@
 package persistence;
 
-public class TextManager {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class TextManager {
+	File f;
+	FileWriter fw;
+	FileReader fr;
+	BufferedWriter bw;
+	BufferedReader br;
+
+	public TextManager(String nameFile) {
+		f = new File(nameFile);
+	}
+
+	public ArrayList<String> readFile(String fileName) throws IOException {
+		ArrayList<String> readLines = new ArrayList<>();
+		fr = new FileReader(fileName);
+		br = new BufferedReader(fr);
+		String readLine;
+		while ((readLine = br.readLine()) != null) {
+			readLines.add(readLine);
+		}
+		br.close();
+		return readLines;
+	}
+	
+	public void writeFile(String nameFile, ArrayList<String> dataList) throws IOException {
+		fw = new FileWriter(nameFile);
+		bw = new BufferedWriter(fw);
+		for (String line : dataList) {
+			bw.write(line);
+			bw.newLine();
+		}
+		bw.flush();
+	}
 }
